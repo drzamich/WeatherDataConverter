4b681199500b52a677b6dc61ded4a93c1815ce1e
Drzamich <drzamich@gmail.com>
2017-12-01 13:31:36 +0100

    2nd commit

--- Station.py
+++ Station.py
@@ -27,47 +27,71 @@ class Station:
             self.endingDates.append(0)
 
 
+observedCharacteristics = [
+    ['air_temperature', 'TU'],
+    ['cloudiness', 'N'],
+    ['precipitation', 'RR'],
+    ['pressure', 'P0'],
+    ['soil_temperature', 'EB'],
+    ['solar', 'ST'],
+    ['sun', 'SD'],
+    ['wind', 'FF']
+]
+
+observations_number = len(observedCharacteristics)
+
+use_recent_data = 0
+
 def createstationlist():
-    observedCharacteristics = [
-        ['air_temperature', 'TU'],
-        ['cloudiness', 'N'],
-        ['precipitation', 'RR'],
-        ['pressure', 'P0'],
-        ['soil_temperature', 'EB'],
-        ['solar', 'ST'],
-        ['sun', 'SD'],
-        ['wind', 'FF']
-    ]
-
-
-    observations_number = 8
-
-    stations_id_list = []
     stations_big_list = []
-    dirpath = os.path.abspath(os.curdir)
+
+    # dirpath = os.path.abspath(os.curdir)
+    dirpath = 'E:\\DOKUMENTY\\WeatherData\\'
 
     for characteristics in observedCharacteristics:
-        a=0
         # Absolute path to the file with station list
-        # Solar data are not separated in recent and historical
+        # Solar data are not separated in recent and historical - therefore the if clause
         if (characteristics[0] != 'solar'):
-            filepath = dirpath + '\\data\\offline\\' + characteristics[0] + '\\historical\\' \
+            folderpath_historical = dirpath + characteristics[0] + '\\historical\\'
+            folderpath_recent = dirpath + characteristics[0] + '\\recent\\'
+            filepath = dirpath + characteristics[0] + '\\historical\\' \
                        + characteristics[1] + '_Stundenwerte_Beschreibung_Stationen.txt'
         else:
-            filepath = dirpath + '\\data\\offline\\' + characteristics[0] + '\\' \
+            folderpath_recent = dirpath + characteristics[0]
+            filepath = dirpath + characteristics[0] + '\\' \
                        + characteristics[1] + '_Stundenwerte_Beschreibung_Stationen.txt'
 
+        # creating list of files in the folder to extract the ending date of "historical" weather data which is "hidden"
+        # in the zip file name
+        #this is also needed to check if the data from the station on the list is actually available as a zip file
+        if characteristics[0] is not 'solar':
+            fileslist_historical = os.listdir(folderpath_historical)
+            idlist_historical = []
+            dateslist_historical = []
+            for file in fileslist_historical:
+                if file.endswith(".zip"):
+                    filename = file.split('_')
+                    idlist_historical.append(filename[2])
+                    dateslist_historical.append(filename[4])
+
+        fileslist_recent = os.listdir(folderpath_recent)
+        idlist_recent = []
+        for file in fileslist_recent:
+            if file.endswith(".zip"):
+                filename = file.split('_')
+                idlist_recent.append(filename[2])
+
+        #reading the file with the station list
         file = open(filepath, 'r')
         whole_file = file.readlines()
         file.close()
         stations_small_list = []
-        observed_ids = []
+
+        historical_list_pointer = 0
         # Skipping first two lines not consistin gof station data
         # Going over every line in the file
-
         for i in range(2, len(whole_file)):
             splitted = whole_file[i].split()  # splitting the single line of the list
-
             # The line of the file has been splitted where empty spaces occur
             # Some stations however have empty spaces in their names
             # The following code deals with that problem
@@ -89,39 +113,48 @@ def createstationlist():
                 for j in range(0, override):
                     del splitted[-1]
 
+            splitted.append(0)  # splitted[8] is_in_historical
+            splitted.append(0)  # splitted[9] date_end_historical
+            splitted.append(0)  # splitted[10] is_in_recent
+
+            if splitted[0] in idlist_historical:
+                splitted[8] = 1
+                splitted[9] = dateslist_historical[historical_list_pointer]
+                historical_list_pointer += 1
+
+            if splitted[0] in idlist_recent:
+                splitted[10] = 1
+
             stations_small_list.append(splitted)
-            stations_id_list.append(splitted[0])
 
         stations_big_list.append(stations_small_list)
 
-    stations_id_list_sorted = []
-    for id in stations_id_list:
-        if id not in stations_id_list_sorted:
-            stations_id_list_sorted.append(id)
 
-    real_stations_list = []
+    return stations_big_list
 
-    saved_ids = []
+
+fulllist = createstationlist()
+
+# Function that creates appropriate list depending on the choosen year
+def yearappropriatelist(year, list):
+    startdate = int(str(year) +'0101')
+    enddate = int(str(year)+'1231')
+    newlist = []
     for i in range(0, observations_number):
-        for station in stations_big_list[i]:
-            if station[0] not in saved_ids:
-                real_stations_list.append([station[0], station[3], station[4], station[5], station[6], station[7]])
+        newlistinner = []
+        for station in list[i]:
+            if(use_recent_data):
+                if int(station[1]) <= startdate and int(station[9]) >= enddate:
+                    newlistinner.append(station)
+            else:
+                if int(station[1]) <= startdate and int(station[2]) >= enddate:
+                    newlistinner.append(station)
+        newlist.append(newlistinner)
+
+    return newlist
 
 
+year = 2016
+yearlist = yearappropriatelist(year, fulllist)
 
-    for i in range(0, observations_number):
-        a=0
-        for id in stations_id_list_sorted:
-            startdate=99999999
-            enddate=0
-            for station in stations_big_list[i]:
-                if id == station [0]:
-                    startdate = station[1]
-                    enddate = station[2]
-
-            real_stations_list[a].append(startdate)
-            real_stations_list[a].append(enddate)
-            a += 1
-
-
-createstationlist()
+print(yearlist[0])
